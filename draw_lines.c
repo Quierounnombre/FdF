5cfbca1c29ac0bbfcda9da01fb3f16d65a6ba8ad
Vicente Garcia Andrade <vicgarci@c1r10s6.42madrid.com>
2023-02-20 15:24:53 +0100

    Solventado limitadores y segfaults, soporte parazoom, error grave con colores deshecho el sistema

--- draw_lines.c
+++ draw_lines.c
@@ -6,29 +6,11 @@
 /*   By: vicgarci <vicgarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 16:41:34 by vicgarci          #+#    #+#             */
-/*   Updated: 2023/02/16 18:12:21 by vicgarci         ###   ########.fr       */
+/*   Updated: 2023/02/20 15:20:53 by vicgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "FdF.h"
-
-static int	get_color_offset(t_vector3D color_pos, uint32_t init_color,
-				t_map *map)
-{
-	int	offset;
-	int	end;
-
-	end = map->map[(int)color_pos.x][(int)color_pos.y].color;
-	offset = end - init_color;
-	if (offset)
-	{
-		ft_printf("\n-----------------------------------------------\n");
-		ft_printf("OFFSET (%d, %d) -> %X\n\n", (int)color_pos.x, (int)color_pos.y, init_color);
-		ft_printf("OFFSET (%d, %d) -> %X\n", (int)color_pos.x + 1, (int)color_pos.y, end);
-		ft_printf("OFFSET (%d, %d) -> %X\n", (int)color_pos.x, (int)color_pos.y + 1, end);
-	}
-	return (offset);
-}
 
 static void	draw_line(t_FdF_info *fdf, t_vector3D v3, t_vector2D v_init,
 				uint32_t init_color)
@@ -37,22 +19,20 @@ static void	draw_line(t_FdF_info *fdf, t_vector3D v3, t_vector2D v_init,
 	float		x_offset;
 	float		y_offset;
 	float		n_dots;
-	int			color_offset;
 
 	v_end = get_iso_perspective(v3, fdf);
 	x_offset = v_end.x - v_init.x;
 	y_offset = v_end.y - v_init.y;
 	n_dots = sqrt(pow(x_offset, 2) + pow(y_offset, 2));
-	color_offset = get_color_offset(v3, init_color, fdf->map);
 	x_offset /= n_dots;
 	y_offset /= n_dots;
-	color_offset /= n_dots;
 	while (n_dots > 0)
 	{
-		mlx_put_pixel(fdf->img, v_init.x, v_init.y, init_color);
+		if (v_init.x >= 0 && v_init.x <= WIDTH && v_init.y >= 0 && v_init.y
+			<= HEIGHT)
+			mlx_put_pixel(fdf->img, v_init.x, v_init.y, init_color);
 		v_init.x += x_offset;
 		v_init.y += y_offset;
-		init_color += color_offset;
 		n_dots--;
 	}
 }
--- get_iso_perspective.c
+++ get_iso_perspective.c
@@ -6,7 +6,7 @@
 /*   By: vicgarci <vicgarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 14:59:40 by vicgarci          #+#    #+#             */
-/*   Updated: 2023/02/15 16:49:07 by vicgarci         ###   ########.fr       */
+/*   Updated: 2023/02/20 15:22:35 by vicgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,14 +23,10 @@ t_vector2D	get_iso_perspective(t_vector3D v3, t_FdF_info *fdf)
 	v_r.x = (v_d->v_alpha.x * v3.x * cam->dim.x)
 		+ (v_d->v_beta.x * v3.y * cam->dim.y)
 		+ (v_d->v_gamma.x * v3.z * cam->dim.z)
-		+ WIDTH / fdf->map->map_size_x;
+		+ WIDTH / 10;
 	v_r.y = (v_d->v_alpha.y * v3.x * cam->dim.x)
 		+ (v_d->v_beta.y * v3.y * cam->dim.y)
 		+ (v_d->v_gamma.y * v3.z * cam->dim.z)
-		+ HEIGHT / fdf->map->map_size_y - 1;
-	if (v_r.y < 0)
-		v_r.y = 0;
-	if (v_r.x < 0)
-		v_r.x = 0;
+		+ HEIGHT / 10;
 	return (v_r);
 }
