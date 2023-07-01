882946a6e96a274c03e5fb75ce020c3043191efe
Vicente Garcia Andrade <vicgarci@c1r11s6.42madrid.com>
2023-07-01 17:53:28 +0200

    PRESENTABLE

--- src/load_angles.c
+++ src/load_angles.c
@@ -6,7 +6,7 @@
 /*   By: vicgarci <vicgarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 13:41:12 by vicgarci          #+#    #+#             */
-/*   Updated: 2023/02/27 14:47:19 by vicgarci         ###   ########.fr       */
+/*   Updated: 2023/05/15 19:30:28 by vicgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,16 @@ void	load_angles(t_angle *ang, t_dir_vectors *v)
 {
 	v->v_alpha.x = cos(ang->alpha);
 	v->v_alpha.y = sin(ang->alpha);
-	ft_printf("V_alpha (%d) x: %d y: %d\n",
-		(int)ang->alpha, (int)v->v_alpha.x, (int)v->v_alpha.y);
+	printf("V_alpha (%f) x: %f y: %f\n",
+		ang->alpha, v->v_alpha.x, v->v_alpha.y);
 	v->v_beta.x = cos(ang->beta);
 	v->v_beta.y = sin(ang->beta);
-	ft_printf("V_beta (%d) x: %d y: %d\n",
-		(int)ang->beta, (int)v->v_beta.x, (int)v->v_beta.y);
+	printf("V_beta (%f) x: %f y: %f\n",
+		ang->beta, v->v_beta.x, v->v_beta.y);
 	v->v_gamma.x = cos(ang->gamma);
 	v->v_gamma.y = sin(ang->gamma);
-	ft_printf("V_gamma (%d) x: %d y: %d\n",
-		(int)ang->gamma, (int)v->v_gamma.x, (int)v->v_gamma.y);
+	printf("V_gamma (%f) x: %f y: %f\n",
+		ang->gamma, v->v_gamma.x, v->v_gamma.y);
 	ft_printf("--------------------\nAngulo iso terminado\n");
 	ft_printf("--------------------\n");
 }
